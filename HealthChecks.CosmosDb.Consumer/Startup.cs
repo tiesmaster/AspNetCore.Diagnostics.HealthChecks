b48c707302d4994b7c44c37383c9077e4389a5cf
Thijs Brobbel <t.brobbel@buddypayment.nl>
2020-07-25 01:49:09 +0200

    TEMP: Add testing for the database existence

--- HealthChecks.CosmosDb.Consumer/Startup.cs
+++ HealthChecks.CosmosDb.Consumer/Startup.cs
@@ -12,7 +12,9 @@ namespace HealthChecks.CosmosDb.Consumer
         {
             services
                 .AddHealthChecks()
-                .AddCosmosDb("AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
+                .AddCosmosDb(
+                "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+                database: "test123");
         }
 
         public void Configure(IApplicationBuilder app)
